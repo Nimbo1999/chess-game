9bfe56af0fd7345eda5ef1a67acbc9bcc9f59b17
Matheus Lopes <matlopes1999@gmail.com>
2022-11-01 11:10:00 -0300

    Update the style of history elements

--- src/components/GameHistory/GameHistory.tsx
+++ src/components/GameHistory/GameHistory.tsx
@@ -1,19 +1,35 @@
 import { Move } from 'chess.js';
 import { useChess } from 'contexts';
+import { useEffect, useRef } from 'react';
 import styles from './GameHistory.module.scss';
 
 const GameHistory: React.FC = () => {
+    const ulref = useRef<HTMLUListElement>(null);
     const { history } = useChess();
+
+    useEffect(() => {
+        const runEffect = () => {
+            if (!ulref.current) return;
+            const ulChildren = ulref.current.children;
+            const lastElement = ulChildren.item(ulChildren.length - 1);
+            if (lastElement)
+                ulref.current.scrollTo({
+                    behavior: 'smooth',
+                    top: (lastElement as HTMLElement).offsetTop,
+                });
+        };
+        runEffect();
+    }, [history]);
 
     return (
         <div className={styles.container}>
             <h3>History</h3>
 
-            <ul>
+            <ul className={styles['container__ul']} ref={ulref}>
                 {history.map((move, i) => {
                     const movement: Move = move as Move;
                     return (
-                        <li key={movement.color + movement.san}>
+                        <li key={movement.color + movement.san + i}>
                             Round {i} - ({movement.color + '-' + movement.san})
                         </li>
                     );
